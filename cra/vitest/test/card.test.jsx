59f2ba5c601cb1a125fa515411301a97e63cdacb
Rhapsody <shalom.agonglo@gmail.com>
2024-02-07 15:22:41 +0100

    pokemon component

--- cra/vitest/test/card.test.jsx
+++ cra/vitest/test/card.test.jsx
@@ -1,7 +1,18 @@
+import React from 'react';
 import { expect, test } from 'vitest';
-import {Card} from "../../remote/src/compenents/card";
+import { render } from '@testing-library/react';
+import Card from "../../remote/src/components/card";
+import { JSDOM } from 'jsdom';
 
-test('The compenent card is loading', () => {
-    console.log('Card', Card)
-//   expect(remoteModule.isInLanding()).toBe(true);
+const dom = new JSDOM();
+global.document = dom.window.document;
+global.window = dom.window;
+
+test('The component card renders a welcome message for the specified user', () => {
+    const userNameTest = "Paul" 
+    const { getByTestId } = render(<Card userName={userNameTest} />);
+    const welcomeMessage = getByTestId('welcome-message');
+
+    expect(welcomeMessage).not.toBeNull();
+    expect(welcomeMessage.textContent).toContain(`Bienvenue ${userNameTest}`);
 });
